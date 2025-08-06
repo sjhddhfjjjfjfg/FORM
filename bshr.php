8eab1ecee42023b410742eade9f65e7fc6a27975
sjhddhfjjjfjfg <sjhddhfjjjfjfg@gmail.com>
2025-08-06 21:47:54 +0500

    Update bshr.php

--- bshr.php
+++ bshr.php
@@ -28,14 +28,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server address
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'hip633253@gmail.com'; // Replace with your email address
-        $mail->Password   = 'gbcb ymna gsns njok'; // Replace with your email password
+        $mail->Username   = 'dndjdbffnfnfnfnnf@gmail.com'; // Replace with your email address
+        $mail->Password   = 'ezzf yhlt eoiq azgg'; // Replace with your email password
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
 
 
         // Email properties
-        $mail->setFrom('hip633253@gmail.com', 'PROFESSOR');
+        $mail->setFrom('dndjdbffnfnfnfnnf@gmail.com', 'PROFESSOR');
         $mail->addAddress('rnxsxnnxnx@gmail.com');
          $mail->addAddress('mahboobalinizamani@gmail.com');
 
